9d434967ff21ebc46cacdd6cfa875312ba8b27ab
kiyoung <popping2606@gmail.com>
2020-09-08 20:54:35 +0900

    jwt: if(token) check username in DB

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -28,21 +28,31 @@ const jwtMiddleware = async (ctx, next) => {
 	
 	try { //토큰을 디코드해서 state.user에 저장
 		const decoded = jwt.verify(token, process.env.JWT_SECRET)
-		ctx.state.user = {
-			_id: decoded._id,
-			username: decoded.username,
+		
+		//토큰은 있으나 DB에 아이디가 없으면 state와 cookies를 제거함
+		const user = await User.findByUsername(decoded.username)
+		if(!user){ 
+			ctx.status = 204 //No content 
+			ctx.cookies.set('access_token', '', cookieOptions)
+			ctx.state.user = null
+			next()
+		}else{
+			ctx.state.user = {
+				_id: decoded._id,
+				username: decoded.username,
+			}
+			//토큰의 남은 유효 기간이 3.5일 미만이면 재발급
+			const now = Math.floor(Date.now() / 1000)
+			if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+				const user = await User.findById(decoded._id)
+				//토큰 재발급
+				const token = user.generateToken()
+				ctx.cookies.set('access_token', token, cookieOptions)
+			}
+	
+			console.log(decoded)
+			return next()
 		}
-		//토큰의 남은 유효 기간이 3.5일 미만이면 재발급
-		const now = Math.floor(Date.now() / 1000)
-		if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
-			const user = await User.findById(decoded._id)
-			//토큰 재발급
-			const token = user.generateToken()
-			ctx.cookies.set('access_token', token, cookieOptions)
-		}
-
-		console.log(decoded)
-		return next()
 	} catch (e) {
 		//토큰 검증 실패
 		console.log('jwt verify fail:', e)
